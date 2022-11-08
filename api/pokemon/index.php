3d69e42de86bae0d2c852d563479bc3888ed829d
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-08 22:10:25 +0100

    controlli method
    CU-3384a98

--- api/pokemon/index.php
+++ api/pokemon/index.php
@@ -1,3 +1,10 @@
 <?php
 require("../headers.php")
+putHeaders("GET"); 
+if($_SERVER['REQUEST_METHOD']=="GET"){
+    
+}else{
+    http_response_code(405);//metodo non permesso
+    echo json_encode(array("message" => "Metodo non abilitato"));
+}
 ?>
