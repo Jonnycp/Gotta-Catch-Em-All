b52eae621b42f169f4e38cfc3c46df1c811d8b4c
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-13 19:00:54 +0000

    CU-32wt933 controllo degli input

--- api/pokemon/index.php
+++ api/pokemon/index.php
@@ -5,6 +5,7 @@ putHeaders("GET");
 if($_SERVER['REQUEST_METHOD']=="GET"){
     if(verificaEsistenzaParametri($_GET)){
         //prossime cose.... soon
+        $valori=controlloInput($_GET);
     }
 }else{
     http_response_code(405);//metodo non permesso
--- api/pokemon/inputs.php
+++ api/pokemon/inputs.php
@@ -2,7 +2,7 @@
 function verificaEsistenzaParametri($array){
     $n=0;
     foreach ($array as $parametro=> $valore) { 
-        $parametri=['HP','ATK','DEF','SP_ATK','SP-DEF','SPEED'];
+        $parametri=['HP','ATK','DEF','SP_ATK','SP_DEF','SPEED'];
         if(in_array(strtoupper($parametro),$parametri) ){
             $n+=1;
         }else{
@@ -19,5 +19,36 @@ if($n<2){
 }else{
     return true;
 }
+}
+
+function controlloInput($array){
+    $inputs=[];
+    //array associativo max e min 
+    $maxmin=array("HP"=>[0, 300],"ATK"=>[0, 300],"DEF"=>[0, 300],"SP_ATK"=>[0, 300],"SP_DEF"=>[0, 300],"SPEED"=>[0, 300]);
+    
+    foreach ($array as $parametro=> $valore) { 
+        $valore = trim($valore); //Toglie gli spazi aggiuntivi davanti e dietro
+        $valore = stripslashes($valore); //Toglie tutti i caratteri esadecimali
+        $valore = htmlspecialchars($valore); //Toglie i caratteri html speciali
+        $valore = str_replace(' ', '', $valore); //Toglie tutti gli spazi
+        if(is_numeric($valore)){
+            $valore=(int)$valore;
+            $min = $maxmin[strtoupper($parametro)][0];
+            $max = $maxmin[strtoupper($parametro)][1];
+            if($valore>= $min && $valore <= $max){
+                $inputs[$parametro]=$valore;  //se esiste la chiave parametro la sostituisco con valore senno te la crea
+            }else{
+                http_response_code(422);
+                echo json_encode(array("message" => "$parametro deve essere compreso tra $min e $max"));   
+            }
+                //se valore è compreso tra max e min con un else con messaggio di errore 
+        }else{
+            http_response_code(422);
+    echo json_encode(array("message" => "$parametro non ha un valore valido"));
+        }
+        //
+
+    }
+    return $inputs;
 }
 ?>
