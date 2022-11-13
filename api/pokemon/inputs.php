9d03ba6fc1f9eae567cb8f178a85655dc131c454
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-13 18:44:35 +0100

    CU-32wta9q lettura input completato

--- api/pokemon/inputs.php
+++ api/pokemon/inputs.php
@@ -1,16 +1,20 @@
 <?php
 function verificaEsistenzaParametri($array){
     $n=0;
-    foreach ($array as $parametro) { 
+    foreach ($array as $parametro=> $valore) { 
         $parametri=['HP','ATK','DEF','SP_ATK','SP-DEF','SPEED'];
-        if(in_array($parametro,$parametri) ){
+        if(in_array(strtoupper($parametro),$parametri) ){
             $n+=1;
+        }else{
+            http_response_code(422);//Unprocessable Entity
+            echo json_encode(array("message" => "Parametro non valido"));
+            return false;
         }
     }
 
 if(n<2){
     http_response_code(422);//Unprocessable Entity
-    echo json_encode(array("message" => "Unprocessable Entity"));
+    echo json_encode(array("message" => "Parametri non sufficienti"));
     return false;
 }else{
     return true;
