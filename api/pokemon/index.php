50947ee7b7c8341c3375b9d0f3885cd3f8bbdca3
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-13 18:15:50 +0000

    CU-32wta9q importato inputs e implementata la funz

--- api/pokemon/index.php
+++ api/pokemon/index.php
@@ -1,8 +1,11 @@
 <?php
 require("../headers.php");
+require("./inputs.php");
 putHeaders("GET"); 
 if($_SERVER['REQUEST_METHOD']=="GET"){
-    
+    if(verificaEsistenzaParametri($_GET)){
+        //prossime cose.... soon
+    }
 }else{
     http_response_code(405);//metodo non permesso
     echo json_encode(array("message" => "Metodo non abilitato"));
