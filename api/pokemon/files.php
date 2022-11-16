0413547ebfe242998de36f5851265600db4592f1
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-16 19:12:46 +0000

    CU-32wt94k lettura del csv completata

--- api/pokemon/files.php
+++ api/pokemon/files.php
@@ -1,19 +1,24 @@
 <?php
 function readCSV(){
     //$file='../../data/pokemons.csv';
-    $file = fopen('data/pokemons.csv', “r”); 
-    $lineNumber = 1;
+    $file = fopen('../../data/pokemons.csv', "r"); 
+    $lineNumber = 0;
     $array=[];
     while(!feof($file)) {
         $stringa = fgets($file);
-        $row = str_getcsv($stringa);//prende la stringa e la suddivide in un array
-        print_r($row);
+        if($lineNumber>0){
+            $row = str_getcsv($stringa);//prende la stringa e la suddivide in un array
+            $pokemon=array("id"=>(int)$row[0],"nome"=>$row[1],"tipo"=>[$row[2],$row[3]],"hp"=>(int)$row[5],"attack"=>(int)$row[6],"defense"=>(int)$row[7],"sp_atk"=>(int)$row[8],"sp_def"=>(int)$row[9],"speed"=>(int)$row[10],"total"=>(int)$row[4],"generazione"=>(int)$row[11],"isLegendary"=>boolval($row[12]));
+            array_push($array,$pokemon);
+
+        }
+        $lineNumber++;
+        
     }
     fclose($file);
-    //$array=array("id"=>[$row],"nome"=>[],"tipo"=>[],"HP"=>[],"ATTACK"=>[],"DEFENSE"=>[],"SP_ATK"=>[],"SP_DEF"=>[],"SPEED"=>[]"TOTAL"=>[],"GENERAZIONE"=>[],"isLegendary"=>[]);
+   
 
     return $array;
 }
 
-readCSV();
 ?>
--- api/pokemon/index.php
+++ api/pokemon/index.php
@@ -1,11 +1,14 @@
 <?php
 require("../headers.php");
 require("./inputs.php");
+require("./files.php");
 putHeaders("GET"); 
 if($_SERVER['REQUEST_METHOD']=="GET"){
     if(verificaEsistenzaParametri($_GET)){
         //prossime cose.... soon
         $valori=controlloInput($_GET);
+        $pokemons=readCSV();
+        echo json_encode($pokemons);
     }
 }else{
     http_response_code(405);//metodo non permesso
