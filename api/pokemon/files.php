65057634d3c5abcf26093c473fd51cff9a591b91
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-17 18:05:22 +0000

    normalizzazione input da finire CU-32wt9c3

--- api/pokemon/files.php
+++ api/pokemon/files.php
@@ -21,4 +21,37 @@ function readCSV(){
     return $array;
 }
 
+function normalizerCSV($pokemon){
+    //richiama ricercaminemax
+    //array che contine pokemon normalizzato con un ciclo for
+    
+}
+
+function ricercaMaxMin($pokemon){
+    $colonne=["hp"=>[],"atk"=>[],"defense"=>[],"sp_atk"=>[],"sp_def"=>[],"speed"=>[]];
+    for ($i=0; $i < count($pokemon); $i++) { 
+        array_push($colonne["hp"],$pokemon[$i]["hp"]);
+        array_push($colonne["atk"],$pokemon[$i]["atk"]);
+        array_push($colonne["defense"],$pokemon[$i]["defense"]);
+        array_push($colonne["sp_atk"],$pokemon[$i]["sp_atk"]);
+        array_push($colonne["sp_def"],$pokemon[$i]["sp_def"]);
+        array_push($colonne["speed"],$pokemon[$i]["speed"]);
+    }
+
+    $minmax=["hp"=>[],"atk"=>[],"defense"=>[],"sp_atk"=>[],"sp_def"=>[],"speed"=>[]];
+    $minhp=min($minmax["hp"]);
+
+    for ($i=0; $i < count($pokemon); $i++) {
+       //array push
+    }
+    
+    //array di due numeri per max e min 
+    //restituire l'array di sopra
+}
+
+function normalizer($min,$max){
+    //applica la formula matematica, prende in input un numero (x)
+    //normalizer chiamta da normalizercsv
+    
+}
 ?>
