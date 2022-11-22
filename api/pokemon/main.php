fdc4f40a56f93db8631d9ce3e95c5cc8387ee064
DalilaAlbanese <92517189+DalilaAlbanese@users.noreply.github.com>
2022-11-22 17:40:25 +0000

    iniziata funzione knn

---
+++ api/pokemon/main.php
@@ -0,0 +1,37 @@
+<?php 
+function distEuclidea(){
+
+}
+
+function matrice($pokemon){
+    $pokDist=[];
+    for ($i=0; $i < count($pokemon); $i++) {
+        array_push();
+    }
+
+
+function ordinamento(){
+    do
+    {
+            $scambiato = false;
+            for( $i = 0, $c = count( $vettore ) - 1; $i < $c; $i++ )
+            {
+                if( $vettore[$i] > $vettore[$i + 1] )
+        {
+                    $temp = $vettore[$i + 1];
+                    $vettore[$i + 1] = $vettore[$i];
+                    $vettore[$i] = $temp;
+                    $scambiato = true;
+        }
+            }
+    }
+    while($scambiato);
+return $vettore;
+} 
+
+function estrazione(){
+    
+}
+
+}
+?>
